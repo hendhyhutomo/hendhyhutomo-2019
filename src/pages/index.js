a11324a79747de3b945e0a2da757233084e5ef25
hendhyhutomo <hendhyhutomo@gmail.com>
2019-03-10 22:54:20 +0700

    update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ export default () => (
 				motion graphic & branding. Since 2015,
 			</p>
 			<p>
-				I have contributed to: Anomali, Segede Gaban, <a href="https://levi.co.id/" target="_blank" rel="noopener noreferrer">Levi's Indonesia</a>, <a href="http://manual.co.id" target="_blank" rel="noopener noreferrer">Manual Jakarta</a>, <a href="http://cindyamelia.com" target="_blank" rel="noopener noreferrer">Cindy Amelia</a>, <a href="https://herbana.id" target="_blank" rel="noopener noreferrer">Herbana</a>, <a href="https://www.museummacan.org" target="_blank" rel="noopener noreferrer">Museum
-				MACAN</a>, <a href="https://thenextcitiesunveiled.manual.co.id/" target="_blank" rel="noopener noreferrer">The Next Cities Unveiled</a>, BIKO group, <a href="https://talentcap.com.my" target="_blank" rel="noopener noreferrer">Talent Cap</a>, Kayu Kayu, <a href="https://alpha.or.id" target="_blank" rel="noopener noreferrer">Alpha Indonesia</a> and many more—
+				I have contributed to: Anomali, Harapan Djaya, BIJI, Curious People, <a href="https://levi.co.id/" target="_blank" rel="noopener noreferrer">Levi's Indonesia</a>, <a href="http://manual.co.id" target="_blank" rel="noopener noreferrer">Manual Jakarta</a>, <a href="http://cindyamelia.com" target="_blank" rel="noopener noreferrer">Cindy Amelia</a>, <a href="https://herbana.id" target="_blank" rel="noopener noreferrer">Herbana</a>, <a href="https://www.museummacan.org" target="_blank" rel="noopener noreferrer">Museum
+				MACAN</a>, <a href="https://thenextcitiesunveiled.manual.co.id/" target="_blank" rel="noopener noreferrer">The Next Cities Unveiled</a>, BIKO group, <a href="https://talentcap.com.my" target="_blank" rel="noopener noreferrer">Talent Cap</a>, Kayu Kayu, <a href="https://alpha.or.id" target="_blank" rel="noopener noreferrer">Alpha Indonesia</a> and&nbsp;many&nbsp;more&nbsp;—
 			</p>
 			<p>
 				Say hello,<wbr /> <a href="mailto:hello@hendhyhutomo.com" target="_blank" rel="noopener noreferrer">hello@hendhyhutomo.com</a>.
