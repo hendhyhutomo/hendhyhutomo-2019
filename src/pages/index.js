395384d1bdb0eb5fdf3080eb2fa5c2063ff041ed
hendhyhutomo <hendhyhutomo@gmail.com>
2019-03-11 11:38:01 +0700

    update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ export default () => (
 		<div className="label">Website is currently under construction.</div>
 		<div className="content">
 			<p>
-				My name is Hendhy Hutomo, I am a digital creatives based in Jakarta, Indonesia. I make website, app,
-				motion graphic & branding. Since 2015,
+				My name is Hendhy Hutomo, I am a digital creative based in Jakarta, Indonesia. I make website, app,
+				motion graphic & branding.
 			</p>
 			<p>
-				I have contributed to: Anomali, Harapan Djaya, BIJI, Curious People, <a href="https://levi.co.id/" target="_blank" rel="noopener noreferrer">Levi's Indonesia</a>, <a href="http://manual.co.id" target="_blank" rel="noopener noreferrer">Manual Jakarta</a>, <a href="http://cindyamelia.com" target="_blank" rel="noopener noreferrer">Cindy Amelia</a>, <a href="https://herbana.id" target="_blank" rel="noopener noreferrer">Herbana</a>, <a href="https://www.museummacan.org" target="_blank" rel="noopener noreferrer">Museum
+				Since 2015, I have contributed to: <wbr />Anomali, Harapan Djaya, BIJI, Curious People, <a href="https://levi.co.id/" target="_blank" rel="noopener noreferrer">Levi's Indonesia</a>, <a href="http://manual.co.id" target="_blank" rel="noopener noreferrer">Manual Jakarta</a>, <a href="http://cindyamelia.com" target="_blank" rel="noopener noreferrer">Cindy Amelia</a>, <a href="https://herbana.id" target="_blank" rel="noopener noreferrer">Herbana</a>, <a href="https://www.museummacan.org" target="_blank" rel="noopener noreferrer">Museum
 				MACAN</a>, <a href="https://thenextcitiesunveiled.manual.co.id/" target="_blank" rel="noopener noreferrer">The Next Cities Unveiled</a>, BIKO group, <a href="https://talentcap.com.my" target="_blank" rel="noopener noreferrer">Talent Cap</a>, Kayu Kayu, <a href="https://alpha.or.id" target="_blank" rel="noopener noreferrer">Alpha Indonesia</a> and&nbsp;many&nbsp;more&nbsp;—
 			</p>
 			<p>
-				Say hello,<wbr /> <a href="mailto:hello@hendhyhutomo.com" target="_blank" rel="noopener noreferrer">hello@hendhyhutomo.com</a>.
+				Say hello, <wbr /><a href="mailto:hello@hendhyhutomo.com" target="_blank" rel="noopener noreferrer">hello@hendhyhutomo.com</a>.
 			</p>
 		</div>
 	</Layout>
