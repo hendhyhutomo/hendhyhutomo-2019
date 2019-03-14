9cb8fab073881c78dcdea73575501d2b435ea1d4
hendhyhutomo <hendhyhutomo@gmail.com>
2019-03-14 20:27:18 +0700

    update content and loading transition

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ export default () => (
 		<div className="label">Website is currently under construction.</div>
 		<div className="content">
 			<p>
-				My name is Hendhy Hutomo, I am a digital creative based in Jakarta, Indonesia. I make website, app,
-				motion graphic & branding.
+				My name is Hendhy Hutomo, I am a digital creative based in Jakarta, Indonesia. I make websites, apps,
+				motion graphics & brand identities.
 			</p>
 			<p>
-				Since 2015,  <wbr />I&nbsp;have contributed to: <wbr />Anomali, Harapan Djaya, BIJI, Curious People, <a href="https://levi.co.id/" target="_blank" rel="noopener noreferrer">Levi's Indonesia</a>, <a href="http://manual.co.id" target="_blank" rel="noopener noreferrer">Manual Jakarta</a>, <a href="http://cindyamelia.com" target="_blank" rel="noopener noreferrer">Cindy Amelia</a>, <a href="https://herbana.id" target="_blank" rel="noopener noreferrer">Herbana</a>, <a href="https://www.museummacan.org" target="_blank" rel="noopener noreferrer">Museum
+				Since 2015,  <wbr />I&nbsp;have contributed to: <wbr />Anomali, <a href="https://www.instagram.com/harapandjaya/" target="_blank" rel="noopener noreferrer">Harapan Djaya</a>, Curious People, <a href="https://levi.co.id/" target="_blank" rel="noopener noreferrer">Levi's Indonesia</a>, <a href="http://manual.co.id" target="_blank" rel="noopener noreferrer">Manual Jakarta</a>, <a href="http://cindyamelia.com" target="_blank" rel="noopener noreferrer">Cindy Amelia</a>, <a href="https://herbana.id" target="_blank" rel="noopener noreferrer">Herbana</a>, <a href="https://www.museummacan.org" target="_blank" rel="noopener noreferrer">Museum
 				MACAN</a>, <a href="https://thenextcitiesunveiled.manual.co.id/" target="_blank" rel="noopener noreferrer">The Next Cities Unveiled</a>, BIKO group, <a href="https://talentcap.com.my" target="_blank" rel="noopener noreferrer">Talent Cap</a>, Kayu Kayu, <a href="https://alpha.or.id" target="_blank" rel="noopener noreferrer">Alpha Indonesia</a> and&nbsp;many&nbsp;more&nbsp;—
 			</p>
 			<p>
