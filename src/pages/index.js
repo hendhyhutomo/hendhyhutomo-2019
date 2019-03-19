8f90dfd6bcdb47c6440f25d5e3b71d737aeb5dc5
hendhyhutomo <hendhyhutomo@gmail.com>
2019-03-20 00:31:50 +0700

    update biko link

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default () => (
 			</p>
 			<p>
 				Since 2015,  <wbr />I&nbsp;have contributed to: <wbr />Anomali, <a href="https://www.instagram.com/harapandjaya/" target="_blank" rel="noopener noreferrer">Harapan Djaya</a>, Curious People, <a href="https://levi.co.id/" target="_blank" rel="noopener noreferrer">Levi's Indonesia</a>, <a href="http://manual.co.id" target="_blank" rel="noopener noreferrer">Manual Jakarta</a>, <a href="http://cindyamelia.com" target="_blank" rel="noopener noreferrer">Cindy Amelia</a>, <a href="https://herbana.id" target="_blank" rel="noopener noreferrer">Herbana</a>, <a href="https://www.museummacan.org" target="_blank" rel="noopener noreferrer">Museum
-				MACAN</a>, <a href="https://thenextcitiesunveiled.manual.co.id/" target="_blank" rel="noopener noreferrer">The Next Cities Unveiled</a>, BIKO group, <a href="https://talentcap.com.my" target="_blank" rel="noopener noreferrer">Talent Cap</a>, Kayu Kayu, <a href="https://alpha.or.id" target="_blank" rel="noopener noreferrer">Alpha Indonesia</a> and&nbsp;many&nbsp;more&nbsp;—
+				MACAN</a>, <a href="https://thenextcitiesunveiled.manual.co.id/" target="_blank" rel="noopener noreferrer">The Next Cities Unveiled</a>, <a href="https://biko-group.com" target="_blank" rel="noopener noreferrer">BIKO group</a>, <a href="https://talentcap.com.my" target="_blank" rel="noopener noreferrer">Talent Cap</a>, Kayu Kayu, <a href="https://alpha.or.id" target="_blank" rel="noopener noreferrer">Alpha Indonesia</a> and&nbsp;many&nbsp;more&nbsp;—
 			</p>
 			<p>
 				Say hello, <wbr /><a href="mailto:hello@hendhyhutomo.com" target="_blank" rel="noopener noreferrer">hello@hendhyhutomo.com</a>.
